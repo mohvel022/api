3409a46650cb516f1fa8eb95ccea4d5c17e09e72
Mohit <mohitveligenti@gmail.com>
2017-02-12 00:23:44 -0800

    point.js res.send()

--- routes/points/points.js
+++ routes/points/points.js
@@ -17,7 +17,10 @@ router.post('/points', function(req, res){
          newPoint.name = req.body.name
          newPoint.xCords = req.body.xCords
          newPoint.yCords = req.body.yCords
-         newPoint.connectedPoints = req.body.connectedPoints
+         var arr = []
+         newPoint.connectedPoints = req.body.connectedPoints.forEach(function(point){
+            arr.push(point)
+         })
          console.log(typeof req.body.connectedPoints)
 
          newPoint.save(function(err){
